453a690846752e72e5a3938fc525446be75abed2
fancyTyphoonKitty <beaupoehls@gmail.com>
2015-07-13 12:00:36 -0700

    notes

--- router.py
+++ router.py
@@ -1,14 +1,3 @@
-import sys
-
-
-"""
-Make sure this syncs to github
-
-"""
-
-def main(
-    sys , name = sys.argv[]
-    )
-print "Hello, " + name
-
-
+router1 = {'os_version':'3.1.1', 'hostname':'nyc_router1', 'model':'nexus 9396', 'domain':'cisco.com', 'mgmt_ip':'10.1.50.11'}
+router2 = dict( os_version='3.2.1', hostname='rtp_router2', model='nexus 9396', domain='cisco.com', mgmt_ip='11.1.50.12')
+router3 = {'os_version':os_version,'hostname': hostname,'model':model,'domain':domain,'mgmt_ip':mgmt_ip}
